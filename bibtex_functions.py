4df39c76b5189dbb81d1643a94a583ac580400e8
Jane Sandberg <sandbej@linnbenton.edu>
2015-06-04 15:43:22 -0400

    adding bibtex entries to marc records before indexing

---
+++ bibtex_functions.py
@@ -0,0 +1,80 @@
+from pymarc import MARCReader
+import re
+
+class BibEntry:
+   def __init__(self, type='book'):
+      #Required elements
+      self.key = 'resource'
+      self.type = type
+      self.authors = []
+      self.publisher = ''
+      self.title = ''
+      self.year = ''
+
+      #Additional elements
+      self.address = ''
+      self.series = ''
+
+   def apply_marc_values(self, record):
+      if record['100']:
+         self.authors.append(record['100']['a'])
+         for field700 in record.get_fields('700'):
+            if field700['a']:
+               self.authors.append(field700['a'])
+      elif record['110']:
+         self.authors.append('{' + record['110']['a'] + '}')
+      elif record['111']:
+         self.authors.append('{' + record['111']['a'] + '}')
+
+      self.title = record.title().replace(' : ', ':').replace(' /', '')
+
+      for field260 in record.get_fields('260'):
+         if field260['b']:
+            self.publisher = field260['b'].replace(',', '')
+            if field260['a']:
+               self.address = field260['a'].replace(' :', '')
+            if field260['c']:
+               match = re.search(r'([12][0-9]{3})', field260['c'])
+               if match:
+                   self.year = match.group(0)
+         else:
+            for field264 in record.get_fields('264'):
+               if field264['b']:
+                  self.publisher = field264['b'].replace(',', '')
+                  if field264['a']:
+                     self.address = field264['a'].replace(' :', '')
+                  if field264['c']:
+                     match = re.search(r'([12][0-9]{3})', field264['c'])
+                     if match:
+                       self.year = match.group(0)
+         
+
+   def apply_sample_values(self):
+      #self.authors.extend(['Mills, M. G. L.', 'Mills, Margie'])
+      self.publisher = 'Jacana Media'
+      self.title = 'Hyena nights & Kalahari days'
+      self.year = '2010'
+      self.address = 'Auckland Park, South Africa'
+      self.series = ''
+
+   def as_bibtex(self):
+      bibtex_entry = []
+      bibtex_entry.append('@')
+      bibtex_entry.append(self.type)
+      bibtex_entry.append('{' + self.key)
+      bibtex_entry.append(self.line('author', (' and '.join(self.authors))))
+      bibtex_entry.append(self.line('publisher', self.publisher))
+      bibtex_entry.append(self.line('title', self.title))
+      bibtex_entry.append(self.line('year', self.year))
+      bibtex_entry.append(self.line('address', self.address))
+      bibtex_entry.append(self.line('series', self.series))
+      bibtex_entry.append('}')
+      return ''.join(bibtex_entry)
+
+   def line(self, label, value):
+      if value is not None and 0 < len(value):
+         if 'year' != label:
+            value = '"' + value + '"'
+         return ', ' + label + ' = ' + value
+      else:
+         return ''
--- prep_oclc.py
+++ prep_oclc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from cleanup_functions import add_fake_marc_formats, add_broadly_defined_corp_bodies, remove_all_fields_with_tag, remove_all_fields_with_tags, remove_bad_subjects, remove_empty_place_of_publication, remove_empty_publisher, remove_field_with_substring, remove_field_with_indicator
 from indexing_functions import index_file
+from bibtex_functions import BibEntry
 from pymarc import MARCReader, Field
 import sys, os
 
@@ -32,78 +33,91 @@ with open(file_name, 'rb') as fh:
          for f in record.get_fields('950'):
             source = f['a']
 
+      bibliography = BibEntry()
+      bibliography.apply_marc_values(record)
+      
+      record.add_field(
+         Field(
+            tag = '951',
+            indicators = [' ', ' '],
+            subfields = [
+               'a', bibliography.as_bibtex()
+            ]))
+
+      
+
       #Vendor-specific cleanup
-         if 'American History in Video' == source:
-            record.leader = record.leader[:6] + 'gz' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.append('655')
+      if 'American History in Video' == source:
+         record.leader = record.leader[:6] + 'gz' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.append('655')
 
-         elif 'American Song' == source:
-            record.leader = record.leader[:6] + 'jz' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.extend(['007', '490', '655'])
-            record.remove_by_substring('500', 'a', 'Compact dis', record)
+      elif 'American Song' == source:
+         record.leader = record.leader[:6] + 'jz' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.extend(['007', '490', '655'])
+         record.remove_by_substring('500', 'a', 'Compact dis', record)
 
-         elif 'Credo Academic Core' == source:
+      elif 'Credo Academic Core' == source:
+         record.leader = record.leader[:6] + 'az' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.append('655')
+
+      elif 'Classical Music Library' == source:
+         record.leader = record.leader[:6] + 'jz' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.extend(['007', '655'])
+
+      elif 'Directory of Open Access Books' == source:
+         record.leader = record.leader[:6] + 'az' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.append('655')
+
+      elif 'ebrary Academic Complete' == source:
+         remove_bad_subjects(record)
+         if 'm' == record.leader[6]:
+            record.leader = record.leader[:6] + 'a' + record.leader[7:]
+         if 'a' == record.leader[6]:
+            record.leader = record.leader[:7] + 'z' + record.leader[8:]
+         remove_field_with_substring('650', 'a', 'Electronic book', record)
+         remove_field_with_indicator('650', 1, '4', record)
+         fields_to_delete.append('655')
+
+      elif ('Films' in source) and ('Demand' in source):
+         record.leader = record.leader[:6] + 'gz' + record.leader[8:]
+         remove_bad_subjects(record)
+         remove_field_with_substring('650', 'a', 'Streaming video', record)
+         for field856 in record.get_fields('856'):
+            field856['u'] = field856['u']+'&cid=1639'
+         if 'TEDTalks' in record['245']['a'] or 'WPA Film Library' in record['245']['a']:
+            if record['245']['b']:
+               record['245']['a'] = record['245']['b']
+         fields_to_delete.extend(['500', '710'])
+
+      elif 'HathiTrust Public Domain only in US Access' == source:
+         remove_bad_subjects(record)
+         if 'a' == record.leader[6] and 'i' == record.leader[7]:
+            record.leader = record.leader[:6] + 'sz' + record.leader[8:]
+         else:
             record.leader = record.leader[:6] + 'az' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.append('655')
-
-         elif 'Classical Music Library' == source:
-            record.leader = record.leader[:6] + 'jz' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.extend(['007', '655'])
-
-         elif 'Directory of Open Access Books' == source:
-            record.leader = record.leader[:6] + 'az' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.append('655')
-
-         elif 'ebrary Academic Complete' == source:
-            remove_bad_subjects(record)
-            if 'm' == record.leader[6]:
-               record.leader = record.leader[:6] + 'a' + record.leader[7:]
-            if 'a' == record.leader[6]:
-               record.leader = record.leader[:7] + 'z' + record.leader[8:]
-            remove_field_with_substring('650', 'a', 'Electronic book', record)
-            remove_field_with_indicator('650', 1, '4', record)
-            fields_to_delete.append('655')
-
-         elif ('Films' in source) and ('Demand' in source):
-            record.leader = record.leader[:6] + 'gz' + record.leader[8:]
-            remove_bad_subjects(record)
-            remove_field_with_substring('650', 'a', 'Streaming video', record)
-            for field856 in record.get_fields('856'):
-               field856['u'] = field856['u']+'&cid=1639'
-            if 'TEDTalks' in record['245']['a'] or 'WPA Film Library' in record['245']['a']:
-               if record['245']['b']:
-                  record['245']['a'] = record['245']['b']
-            fields_to_delete.extend(['500', '710'])
-
-         elif 'HathiTrust Public Domain only in US Access' == source:
-            remove_bad_subjects(record)
-            if 'a' == record.leader[6] and 'i' == record.leader[7]:
-               record.leader = record.leader[:6] + 'sz' + record.leader[8:]
-            else:
-               record.leader = record.leader[:6] + 'az' + record.leader[8:]
-            remove_field_with_substring('650', 'a', 'Theses', record)
-            remove_field_with_indicator('650', 1, '4', record)
-            fields_to_delete.append('655')
+         remove_field_with_substring('650', 'a', 'Theses', record)
+         remove_field_with_indicator('650', 1, '4', record)
+         fields_to_delete.append('655')
             
-         elif 'NCBI Bookshelf' == source:
-            record.leader = record.leader[:6] + 'az' + record.leader[8:]
-            remove_bad_subjects(record)
-            fields_to_delete.append('007', '655')
+      elif 'NCBI Bookshelf' == source:
+         record.leader = record.leader[:6] + 'az' + record.leader[8:]
+         remove_bad_subjects(record)
+         fields_to_delete.append('007', '655')
 
-         elif 'Smithsonian Global Sound For Libraries' == source:
-            record.leader = record.leader[:6] + 'jz' + record.leader[8:]
-            fields_to_delete.append('007')
+      elif 'Smithsonian Global Sound For Libraries' == source:
+         record.leader = record.leader[:6] + 'jz' + record.leader[8:]
+         fields_to_delete.append('007')
 
-         elif 'Wright American Fiction' == source:
-            record.leader = record.leader[:6] + 'az' + record.leader[8:]
-            remove_field_with_indicator('655', 1, '4', record)
+      elif 'Wright American Fiction' == source:
+         record.leader = record.leader[:6] + 'az' + record.leader[8:]
+         remove_field_with_indicator('655', 1, '4', record)
 
-         remove_all_fields_with_tags(fields_to_delete, record)
-         out.write(record.as_marc())
+      remove_all_fields_with_tags(fields_to_delete, record)
+      out.write(record.as_marc())
    out.close()
    index_file(output_file_name)
