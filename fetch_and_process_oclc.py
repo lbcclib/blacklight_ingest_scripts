e9e6488cfbe438cb2063315b6a9944cb0517f2d9
Jane Sandberg <sandbergja@users.noreply.github.com>
2015-09-01 16:06:24 -0700

    rewriting to get everything from past week

--- fetch_and_process_oclc.py
+++ fetch_and_process_oclc.py
@@ -3,42 +3,47 @@
 from ftplib import FTP
 import os, time
 import oclc_credentials
-
-def get_filename(data):
-   file_dict = {}
-   for line in data:
-      col_list = line.split()
-      date_str = ' '.join(line.split()[5:8])
-      file_dict[time.strptime(date_str, '%b %d %H:%M')] = col_list[8]
-      date_list = list([key for key, value in file_dict.items()])
-   return file_dict[max(date_list)]
+from datetime import strptime, date, timedelta
 
 ftp = FTP(oclc_credentials.server)
 ftp.login(oclc_credentials.username, oclc_credentials.password)
 
 #New files
 ftp.cwd('metacoll/out/ongoing/new')
-data = []
-ftp.dir(data.append)
-filename = get_filename(data)
-ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/data/new/%s' % filename, 'a+').write)
-os.system('python ~/scripts/prep_oclc.py ~/data/new/' + filename)
+files = []
+ftp.dir(files.append)
+for line in files:
+   col_list = line.split()
+   date_str = ' '.join(line.split()[5:8])
+   file_date = datetime.strptime(date_str, '%b %d %H:%M').date().replace(date.today().year)
+   if ((date.today() - from_ftp.date()) < timedelta(weeks=1)):
+      filename = col_list[8]
+      ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/data/new/%s' % filename, 'a+').write)
+      os.system('python ~/scripts/prep_oclc.py ~/data/new/' + filename)
 
 
 #Update files
 ftp.cwd('../updates')
-data = []
+files = []
 ftp.dir(data.append)
-filename = get_filename(data)
-ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/data/update/%s' % filename, 'a+').write)
-os.system('python ~/scripts/prep_oclc.py ~/data/update/' + filename)
+for line in files:
+   col_list = line.split()
+   date_str = ' '.join(line.split()[5:8])
+   file_date = datetime.strptime(date_str, '%b %d %H:%M').date().replace(date.today().year)
+   if ((date.today() - from_ftp.date()) < timedelta(weeks=1)):
+      filename = col_list[8]
+      ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/update/%s' % filename, 'a+').write)
+      os.system('python ~/scripts/prep_oclc.py ~/data/update/' + filename)
 
 #Delete files
 ftp.cwd('../deletes')
-data = []
+files = []
 ftp.dir(data.append)
-filename = get_filename(data)
-ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/data/delete/%s' % filename, 'a+').write)
-os.system('python ~/scripts/delete_oclc.py ~/data/delete/' + filename)
-
-#but this doesn't quite work if multiple files in one session
+for line in files:
+   col_list = line.split()
+   date_str = ' '.join(line.split()[5:8])
+   file_date = datetime.strptime(date_str, '%b %d %H:%M').date().replace(date.today().year)
+   if ((date.today() - from_ftp.date()) < timedelta(weeks=1)):
+      filename = col_list[8]
+      ftp.retrbinary('RETR %s' % filename, open('/home/lbccadmin/delete/%s' % filename, 'a+').write)
+      os.system('python ~/scripts/delete_oclc.py ~/data/delete/' + filename)
